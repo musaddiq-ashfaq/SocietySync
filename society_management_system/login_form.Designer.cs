f17220676817c10085d280b09978049d515afff0
musaddiq-ashfaq <musasaddiq029@gmail.com>
2024-03-20 00:32:44 +0500

    added login signup

--- society_management_system/login_form.Designer.cs
+++ society_management_system/login_form.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace society_management_system
 {
-    partial class Form1
+    partial class login_form
     {
         /// <summary>
         /// Required designer variable.
@@ -31,103 +31,88 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.username_box = new System.Windows.Forms.TextBox();
+            this.login_button = new System.Windows.Forms.Button();
             this.password_box = new System.Windows.Forms.TextBox();
-            this.signup_button = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label1.Location = new System.Drawing.Point(166, 106);
+            this.label1.Location = new System.Drawing.Point(222, 108);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter Username:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label2.Location = new System.Drawing.Point(170, 184);
+            this.label2.Location = new System.Drawing.Point(222, 192);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.Size = new System.Drawing.Size(84, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter Password:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // username_box
             // 
-            this.username_box.Location = new System.Drawing.Point(318, 103);
+            this.username_box.Location = new System.Drawing.Point(400, 101);
             this.username_box.Name = "username_box";
-            this.username_box.Size = new System.Drawing.Size(150, 20);
+            this.username_box.Size = new System.Drawing.Size(178, 20);
             this.username_box.TabIndex = 2;
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(319, 268);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(75, 23);
+            this.login_button.TabIndex = 4;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
             // 
             // password_box
             // 
-            this.password_box.Location = new System.Drawing.Point(318, 181);
+            this.password_box.Location = new System.Drawing.Point(400, 185);
             this.password_box.Name = "password_box";
-            this.password_box.Size = new System.Drawing.Size(150, 20);
-            this.password_box.TabIndex = 3;
-            // 
-            // signup_button
-            // 
-            this.signup_button.Location = new System.Drawing.Point(318, 254);
-            this.signup_button.Name = "signup_button";
-            this.signup_button.Size = new System.Drawing.Size(92, 33);
-            this.signup_button.TabIndex = 4;
-            this.signup_button.Text = "Signup";
-            this.signup_button.UseVisualStyleBackColor = true;
-            this.signup_button.Click += new System.EventHandler(this.signup_button_Click);
+            this.password_box.Size = new System.Drawing.Size(178, 20);
+            this.password_box.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
-            this.label3.Location = new System.Drawing.Point(333, 41);
+            this.label3.Location = new System.Drawing.Point(270, 313);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 22);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "WELCOME";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(298, 333);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(132, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Already have an account?";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Don\'t have an account?";
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(430, 333);
+            this.linkLabel1.Location = new System.Drawing.Point(398, 313);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(33, 13);
-            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel1.TabIndex = 7;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Login";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
+            this.linkLabel1.Text = "Signup";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
-            // Form1
+            // login_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.signup_button);
             this.Controls.Add(this.password_box);
+            this.Controls.Add(this.login_button);
             this.Controls.Add(this.username_box);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "login_form";
+            this.Text = "login_form";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,11 +123,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox username_box;
+        private System.Windows.Forms.Button login_button;
         private System.Windows.Forms.TextBox password_box;
-        private System.Windows.Forms.Button signup_button;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
-
--- society_management_system/login_form.cs
+++ society_management_system/login_form.cs
@@ -2,52 +2,39 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace society_management_system
 {
-    public partial class Form1 : Form
+    public partial class login_form : Form
     {
         SqlConnection connection_string = new SqlConnection(@"Data Source=DESKTOP-LIM5U3M\SQLEXPRESS;Initial Catalog=society_db;Integrated Security=True");
-        public Form1()
+        public login_form()
         {
             InitializeComponent();
         }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void signup_button_Click(object sender, EventArgs e)
+        private void login_button_Click(object sender, EventArgs e)
         {
             String username = username_box.Text;
             String password = password_box.Text;
-
-             bool flag = InsertUser(username,password);
-            if (flag == false)
+            bool flag = check_user(username, password);
+            if(flag==true)
             {
-                MessageBox.Show("Username Already Exist");
+                MessageBox.Show("Login Successful");
             }
             else
             {
-                MessageBox.Show("SignUp Successful");
+                MessageBox.Show("Check username and password");
             }
         }
-        private bool InsertUser(string username, string password)
+        private bool check_user(string username, string password)
         {
-            string query = @"
-            INSERT INTO users (username, password)
-            SELECT @username, @password
-            WHERE NOT EXISTS (
-                SELECT 1 FROM users WHERE username = @username
-            )";
-
+            string query = @"SELECT COUNT(*) FROM users WHERE username = @username AND password = @password";
             using (SqlConnection connection = new SqlConnection(connection_string.ConnectionString))
             {
                 SqlCommand command = new SqlCommand(query, connection);
@@ -57,12 +44,12 @@ namespace society_management_system
                 try
                 {
                     connection.Open();
-                    int rowsAffected = command.ExecuteNonQuery();
-                    return rowsAffected > 0;
+                    int count = (int)command.ExecuteScalar();
+                    return count > 0;
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Error inserting user: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Error checking user credentials: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return false;
                 }
             }
@@ -70,13 +57,8 @@ namespace society_management_system
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-
-        }
-
-        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            login_form loginForm = new login_form();
-            loginForm.Show();
+            Form1 signup_form = new Form1();
+            signup_form.Show();
             this.Hide();
         }
     }
